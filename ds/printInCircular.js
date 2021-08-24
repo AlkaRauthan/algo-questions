77a439867e459aba9a7f8349b5365be53db0d489
Alka Rauthan <rauthanalka@gmail.com>
2021-08-24 11:46:57 +0530

    Create printInCircular.js

---
+++ ds/printInCircular.js
@@ -0,0 +1,23 @@
+var array = [[ 1,2,3,4,5],
+            [3,4,5,6,7,8],
+            [3,4,5,6,7,8],  
+            [3,4,5,6,7,8],
+            [3,4,5,6,7,8]
+            ];
+
+            len = 5;
+            colLength = 5
+            rowcounter=0 colcounter=4
+            1 3
+            2 2
+            3 1
+ while (rowcounter < colcounter){
+     row = rowcounter ; col = colcounter;
+            for 0th row print array col++ till col < colcounter
+            if last col == len -1 print col till row < length row++ col same
+            if row == len -1 && row == len -1 while col >= 0 col --
+            if row == len -1 col ==0 row -- while len -2
+
+            rowcounter++ colcounter--
+
+ }
